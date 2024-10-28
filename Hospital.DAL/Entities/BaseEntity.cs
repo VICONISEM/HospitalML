7e01b895cb4ae77c2e45ec8573360b80d210c3fc
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-10-28 22:13:03 +0300

    BaseEntity Creation

---
+++ Hospital.DAL/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.DAL.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }= DateTime.Now;
+
+        public bool IsDeleted { get; set; } = false; 
+
+    }
+}
