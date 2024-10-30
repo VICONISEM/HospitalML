8a27d20dca4994da767cd2861613b5732da59301
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-10-30 20:51:56 +0300

    AddRelationOfpatientAndBI

--- Hospital.DAL/Configurations/PatientCongiguration.cs
+++ Hospital.DAL/Configurations/PatientCongiguration.cs
@@ -15,9 +15,17 @@ namespace Hospital.DAL.Configurations
         {
             builder.HasKey(p => p.Id);
 
-            builder.Property(p => p.Id).UseIdentityColumn(1, 1);
+            builder.Property(p => p.Id)
+                .UseIdentityColumn(1, 1);
 
             builder.HasIndex(p => p.Name);
+
+            builder.HasMany(p => p.biologicalIndicators)
+                .WithOne(BI => BI.paient)
+                .HasForeignKey(BI => BI.PatientId);
+
+
+
 
 
 
