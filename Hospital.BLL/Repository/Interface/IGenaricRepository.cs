dd8da92dfdc325585aa73a50e2b9926463a69d56
AmMaR1922 <ammar191230@gmail.com>
2024-10-31 20:46:36 +0300

    UPdate IGenaricRepository  CRUD

--- Hospital.BLL/Repository/Interface/IGenaricRepository.cs
+++ Hospital.BLL/Repository/Interface/IGenaricRepository.cs
@@ -13,6 +13,12 @@ namespace Hospital.BLL.Repository.Interface
     {
         List<TEntity>GetAll();
 
+         TEntity GetById(int id);    
+
+        void Add(TEntity entity); 
+        
+        void Update(TEntity entity);    
+        void Delete(TEntity entity);    
        
         
     }
