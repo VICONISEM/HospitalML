6b7c029db52a20157b65cae14849a8afe8f18fee
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-10-31 21:02:51 +0300

    TestTwoSync

--- Hospital.BLL/Repository/GenericRepository.cs
+++ Hospital.BLL/Repository/GenericRepository.cs
@@ -10,16 +10,47 @@ using System.Threading.Tasks;
 
 namespace Hospital.BLL.Repository
 {
-    public class GenericRepository<TEntity> : IGenaricRepository<TEntity> where TEntity : BaseEntity
-    {
-        private readonly HospitalDbContext _context;
-        public GenericRepository(HospitalDbContext context)
-        {
-            _context = context;
-        }
-       
-        public List<TEntity> GetAll()
-        => _context.Set<TEntity>().ToList();
-       
-    }
+
+	public class GenericRepository<TEntity> : IGenaricRepository<TEntity> where TEntity : BaseEntity
+	{
+		private readonly HospitalDbContext _context;
+		public GenericRepository(HospitalDbContext context)
+		{
+			_context = context;
+		}
+
+
+		//  public GenericRepository() { }
+
+		public List<TEntity> GetAll()
+	         => _context.Set<TEntity>().ToList();
+
+		public void Add(TEntity entity)
+		{
+			_context.Set<TEntity>().Add(entity);
+
+		}
+
+		public void Update(TEntity entity)
+		{
+			_context.Set<TEntity>().Update(entity);
+		}
+
+
+		public TEntity? GetById(int? id)
+		{  
+			return  _context.Set<TEntity>().FirstOrDefault(x => x.Id == id);
+		}
+
+
+
+		public void Delete(TEntity entity)
+		{
+		 _context.Set<TEntity>().Remove(entity);
+		}
+
+
+
+	}
+
 }
