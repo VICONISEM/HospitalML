3428b8d041c56687b87286515ae696fd8f20329a
Mohamed Essam <Mohamed Essam@DESKTOP-SJSM9SF>
2024-11-14 21:48:22 +0200

    comitaya

--- HospitalML/Controllers/DashboardController.cs
+++ HospitalML/Controllers/DashboardController.cs
@@ -12,12 +12,12 @@ namespace HospitalML.Controllers
     [Route("api/[controller]")]
     [ApiController]
     [Authorize(Roles = "Admin")]
-    public class DashboardController : ControllerBase
+    public class HospitalController : ControllerBase
     {
         private readonly IGenaricRepository<Patient> PatientRepo;
         private readonly IGenaricRepository<Hospitals> HospitalRepo;
 
-        public DashboardController(IGenaricRepository<Patient> PatientRepo, IGenaricRepository<Hospitals> HospitalRepo)
+        public HospitalController(IGenaricRepository<Patient> PatientRepo, IGenaricRepository<Hospitals> HospitalRepo)
         {
             this.PatientRepo = PatientRepo;
             this.HospitalRepo = HospitalRepo;
@@ -46,7 +46,55 @@ namespace HospitalML.Controllers
 
             if (hospital == null) return BadRequest();
 
-            await HospitalRepo.Delete(hospital);
+            var result = await HospitalRepo.Delete(hospital);
+
+            if(result == 0) return BadRequest();
+
+            return Ok();
+        }
+
+        [HttpPost("UpdateHospital")]
+        public async Task<ActionResult<HospitalDto>> UpdateHospital(HospitalDto hospitalDto, int Id)
+        {
+            var hospital = new Hospitals()
+            {
+                Name = hospitalDto.Name,
+                Address = hospitalDto.Address,
+                City = hospitalDto.City,
+                Country = hospitalDto.Country,
+                Id = Id
+            };
+
+            var Result = await HospitalRepo.Update(hospital);
+
+            if (Result == 0) return BadRequest();
+
+
+
+            return Ok(hospitalDto);
+        }
+
+        [HttpGet("GetHospitals")]
+        public async Task<ActionResult<List<HospitalDto>>> GetAllHospitals()
+        {
+            var hospitals = await HospitalRepo.GetAll();
+
+            var Result = new List<HospitalDto>();
+
+            foreach (var hospital in hospitals)
+            {
+                var hospitalDto = new HospitalDto()
+                {
+                    Address = hospital.Address,
+                    Name = hospital.Name,
+                    City = hospital.City,
+                    Country = hospital.Country,
+                    Id = hospital.Id
+                };
+                Result.Add(hospitalDto);
+            }
+
+            return Ok(Result);
         }
 
     }
---
+++ HospitalML/Controllers/PatientController.cs
@@ -0,0 +1,23 @@
+﻿using Hospital.BLL.Repository.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalML.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class PatientController : ControllerBase
+    {
+        private readonly IPatientRepository patientRepo;
+
+        public PatientController(IPatientRepository PatientRepo)
+        {
+            patientRepo = PatientRepo;
+        }
+
+        //[HttpGet("GetAllPatients")]
+
+    }
+}
