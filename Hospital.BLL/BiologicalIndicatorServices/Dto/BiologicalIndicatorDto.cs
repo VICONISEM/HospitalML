8137a5f2084b419953e4885d597fc8bc132b1469
Mohamed Essam <Mohamed Essam@DESKTOP-SJSM9SF>
2024-11-15 02:08:36 +0200

    comitttt

--- Hospital.BLL/BiologicalIndicatorServices/Dto/BiologicalIndicatorDto.cs
+++ Hospital.BLL/BiologicalIndicatorServices/Dto/BiologicalIndicatorDto.cs
@@ -9,9 +9,9 @@ namespace Hospital.BLL.BiologicalIndicatorServices.Dto
 {
 	public class BiologicalIndicatorDto
 	{
-        public int Id { get; set; }
-        public int HealthConditionScore { get; set; }
-		public string HealthCondition { get; set; }
+        public int? Id { get; set; }
+        public int? HealthConditionScore { get; set; }
+		public string? HealthCondition { get; set; }
 		public float SugarPercentage { get; set; }
 		public float BloodPressure { get; set; }
 		public float AverageTemprature { get; set; }
